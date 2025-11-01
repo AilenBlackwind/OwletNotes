e51750a5a878f1b36b9d4123c45d162c85558278
AilenBlackwind <134120935+AilenBlackwind@users.noreply.github.com>
2025-11-01 09:15:56 +0700

    BigC

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,20 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      publicDir: 'public', 
+      publicDir: 'public',
+      build: {
+        rollupOptions: {
+          input: {
+            main: path.resolve(__dirname, 'index.html'),
+            background: path.resolve(__dirname, 'src/background.js'),
+          },
+          output: {
+            entryFileNames: (chunkInfo) => {
+              return chunkInfo.name === 'background' ? '[name].js' : 'assets/[name]-[hash].js';
+            }
+          }
+        },
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
